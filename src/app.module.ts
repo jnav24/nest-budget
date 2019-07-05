f696f0c85152fabbbbb9c65dd7c3a534fb3ed916
Justin Navarro <info@justinnavarro.net>
2019-07-05 17:23:22 -0400

    add typeorm config back

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,17 +4,36 @@ import { AppService } from './app.service';
 import {ConfigModule} from './config/config.module';
 import { TestController } from './test/test.controller';
 import * as dotenv from 'dotenv';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {Connection} from 'typeorm';
+import {VehicleTypesService} from './services/vehicle-types.service';
+import {VehiclesTypesEntity} from './entities/vehicles-types.entity';
 
 dotenv.config({ path: './.env' });
 
 @Module({
   imports: [
       ConfigModule,
+      TypeOrmModule.forRoot({
+          type: 'mysql',
+          host: process.env.DATABASE_HOST,
+          port: 3306,
+          username: process.env.DATABASE_USER,
+          password: process.env.DATABASE_PASSWORD,
+          database: process.env.DATABASE_NAME,
+          entities: [__dirname + '/**/*.entity{.ts,.js}'],
+          synchronize: true,
+      }),
+      TypeOrmModule.forFeature([
+          VehiclesTypesEntity,
+      ]),
   ],
   controllers: [AppController, TestController],
   providers: [
       AppService,
+      VehicleTypesService,
   ],
 })
 export class AppModule {
+    constructor(private readonly connection: Connection) {}
 }
