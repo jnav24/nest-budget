329dd9ff056c3049df16f853914f94238020de10
Justin Navarro <info@justinnavarro.net>
2019-07-04 22:53:44 -0400

    add typeorm with configuration into the app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,27 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {ConfigModule} from './config/config.module';
+import { TestController } from './test/test.controller';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {Connection} from 'typeorm';
 
 @Module({
-  imports: [ConfigModule],
-  controllers: [AppController],
+  imports: [
+      ConfigModule,
+      TypeOrmModule.forRoot({
+          type: 'mysql',
+          host: process.env.DATABASE_HOST,
+          port: 3306,
+          username: process.env.DATABASE_USER,
+          password: process.env.DATABASE_PASSWORD,
+          database: process.env.DATABASE_NAME,
+          entities: [__dirname + '/**/*.entity{.ts,.js}'],
+          synchronize: true,
+      }),
+  ],
+  controllers: [AppController, TestController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+    constructor(private readonly connection: Connection) {}
+}
