cecd3cf9dba37794173535d4f2b63799f3ef1108
Justin Navarro <info@justinnavarro.net>
2019-07-08 16:42:41 -0400

    hash user password

--- src/test/test.controller.ts
+++ src/test/test.controller.ts
@@ -3,11 +3,13 @@ import {ConfigService} from '../config/config.service';
 import {VehicleTypesService} from '../services/vehicle-types.service';
 import {UserService} from '../services/user.service';
 import {UserDto} from '../dto/user.dto';
+import {HashService} from '../services/hash.service';
 
 @Controller('test')
 export class TestController {
     constructor(
         private readonly configService: ConfigService,
+        private readonly hashService: HashService,
         private readonly userService: UserService,
         private readonly vehicleTypesService: VehicleTypesService,
     ) {}
@@ -31,10 +33,11 @@ export class TestController {
     @Post('user')
     async testPostUser(@Body() userDto: UserDto) {
         try {
+            const user = { ...userDto, password: await this.hashService.getHash(userDto.password) };
             return {
                 status: 200,
                 msg: 'yippe!',
-                data: await this.userService.create(userDto),
+                data: await this.userService.create(user),
             };
         } catch (error) {
             // console.log(error);
