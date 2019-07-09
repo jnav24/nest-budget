0fb1be8a2bc73e7e569de5898a64621b06db6f41
Justin Navarro <info@justinnavarro.net>
2019-07-08 23:18:07 -0400

    functionality for validation pipe

--- src/shared/pipes/validation.pipe.ts
+++ src/shared/pipes/validation.pipe.ts
@@ -1,8 +1,34 @@
-import {ArgumentMetadata, Injectable, PipeTransform} from '@nestjs/common';
+import {ArgumentMetadata, BadRequestException, Injectable, PipeTransform} from '@nestjs/common';
+import {plainToClass} from 'class-transformer';
+import {validate} from 'class-validator';
 
 @Injectable()
 export class ValidationPipe implements PipeTransform {
-    transform(value: any, metadata: ArgumentMetadata) {
-        // ...
+    async transform(value: any, { metatype }: ArgumentMetadata) {
+        if (value instanceof Object && this.isEmpty(value)) {
+            throw new BadRequestException('Validation Failed: Empty State');
+        }
+
+        if (!metatype || !this.toValidate(metatype)) {
+            return value;
+        }
+
+        const obj = plainToClass(metatype, value);
+        const errors = await validate(obj);
+
+        if (errors.length) {
+            throw new BadRequestException('Validation Failed');
+        }
+
+        return value;
+    }
+
+    private toValidate(metatype): boolean {
+        const types =  [String, Boolean, Number, Array, Object];
+        return !types.find((type) => metatype === type);
+    }
+
+    private isEmpty(obj: { [key: string]: any }) {
+        return !Object.keys(obj);
     }
 }
