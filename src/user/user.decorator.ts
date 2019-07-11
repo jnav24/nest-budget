e89ca2bf0bb0eb8917b3cdbf623c178919d97501
Justin Navarro <info@justinnavarro.net>
2019-07-11 18:26:31 -0400

    remove console.log

--- src/user/user.decorator.ts
+++ src/user/user.decorator.ts
@@ -1,6 +1,5 @@
 import {createParamDecorator} from '@nestjs/common';
 
 export const User = createParamDecorator((data, req) => {
-    console.log(req.user);
     return data ? req.user[data] : req.user;
 });
