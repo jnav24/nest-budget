53ea35e67f764666ac3fda3ad9803b082d2c11f7
Justin Navarro <info@justinnavarro.net>
2019-07-11 23:20:57 -0400

    removed unused service

--- src/jwt-auth/jwt-auth.controller.ts
+++ src/jwt-auth/jwt-auth.controller.ts
@@ -2,7 +2,6 @@ import {Body, Controller, HttpException, HttpStatus, Post, UsePipes} from '@nest
 import {JwtAuthService} from './jwt-auth.service';
 import {UserDto} from '../user/user.dto';
 import {ValidationPipe} from '../shared/pipes/validation.pipe';
-import {ConfigService} from '../config/config.service';
 import {UserService} from '../user/user.service';
 import {HashService} from '../shared/services/hash.service';
 import {UserInterface} from '../user/user.interface';
@@ -13,12 +12,9 @@ export class JwtAuthController {
 
     constructor(
         private readonly authService: JwtAuthService,
-        private readonly config: ConfigService,
         private readonly hashService: HashService,
         private readonly userService: UserService,
-    ) {
-        this.env = config.read();
-    }
+    ) {}
 
     @Post('login')
     @UsePipes(new ValidationPipe())
